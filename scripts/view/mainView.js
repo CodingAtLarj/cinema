5aeef179875305b5e3500899d8cf5349e2f93712
Rami Bououni <rami.bououni@gmail.com>
2017-08-03 12:46:51 -0700

    fixed showing the appropriate main page, and logic to check for users

--- scripts/view/mainView.js
+++ scripts/view/mainView.js
@@ -1,9 +1,5 @@
 'use strict'
 
-getMovies();
-getFavorites();
-getUsers();
-
 $('#resultsList *').children('img.stylePhoto').addClass('tiny')
 $('#userList *').children('img.stylePhoto').addClass('frontPhotos')
 $('img.stylePhoto').append('<span></span>')
